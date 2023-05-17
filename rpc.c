8b3fe81beb3b08493e27f47887ad2359099cc237
cxlanyagege <cxlanyagege@hotmail.com>
2023-05-17 19:32:09 +1000

    create server and socket when calling rpc_init_server

--- rpc.c
+++ rpc.c
@@ -1,21 +1,67 @@
-#include "rpc.h"
+#define _POSIX_C_SOURCE 200112L
+#include <netdb.h>
+#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include "rpc.h"
+
 
 struct rpc_server {
     /* Add variable(s) for server state */
+    int port;                           // Server port
+    int socket_fd;                      // Server socket
+    int socket_opt;                     // Socket option
+    struct addrinfo hint, *res, *rp;    // Address infomation
 };
 
 rpc_server *rpc_init_server(int port) {
-    return NULL;
+    /* Allocate memory to server */
+    rpc_server *server = malloc(sizeof(rpc_server));
+
+    /* Check if server port is valid */
+    if (port <= 0) {
+        return NULL;
+    } else {
+        server->port = port;
+    }
+
+    /* Get address informations */
+    server->socket_fd = 0;
+    server->socket_opt = 1;
+    memset(&server->hint, 0, sizeof(server->hint));
+    server->hint.ai_family = AF_INET;
+    server->hint.ai_socktype = SOCK_STREAM;
+    server->hint.ai_flags = AI_PASSIVE;
+
+    /* Create socket for server */
+    char port_buffer[11];
+    sprintf(port_buffer, "%d", port);
+    getaddrinfo(NULL, port_buffer, &server->hint, &server->res);
+    server->socket_fd = socket(server->res->ai_family, 
+                               server->res->ai_socktype, 
+                               server->res->ai_flags);
+
+    /* Set socket options */
+    setsockopt(server->socket_fd, SOL_SOCKET, SO_REUSEADDR, 
+              &server->socket_opt, sizeof(server->socket_opt));
+
+    return server;
 }
 
 int rpc_register(rpc_server *srv, char *name, rpc_handler handler) {
-    return -1;
+    /* Check if arguments are valid */
+    if (srv == NULL || name == NULL || handler == NULL) {
+        return -1;
+    }
+
+    return 0;
 }
 
 void rpc_serve_all(rpc_server *srv) {
 
 }
+
 
 struct rpc_client {
     /* Add variable(s) for client state */
--- rpc.h
+++ rpc.h
@@ -6,6 +6,7 @@
 
 #include <stddef.h>
 
+
 /* Server state */
 typedef struct rpc_server rpc_server;
 /* Client state */
